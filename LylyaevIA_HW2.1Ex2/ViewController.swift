b5a30d3f77e449785a7878e5271d537d0e2e42b3
Lertu <Lertu>
2021-01-28 08:34:51 +0300

    Exercise 2

--- LylyaevIA_HW2.1Ex2/ViewController.swift
+++ LylyaevIA_HW2.1Ex2/ViewController.swift
@@ -8,12 +8,43 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet var redLightView: UIView!
+    @IBOutlet var yellowLightView: UIView!
+    @IBOutlet var greenLightView: UIView!
+    @IBOutlet var changeLightButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.alpha = 0.3
+        yellowLightView.alpha = 0.3
+        greenLightView.alpha = 0.3
+        redLightView.layer.cornerRadius = 50
+        yellowLightView.layer.cornerRadius = 50
+        greenLightView.layer.cornerRadius = 50
+        changeLightButton.layer.cornerRadius = 10
+        
     }
-
-
+    
+    
+    @IBAction func changeLightButtonPressed(_ sender: Any) {
+        changeLightButton.setTitle("NEXT", for: .normal)
+        if redLightView.alpha == 0.3 {
+            redLightView.alpha = 1
+        }
+        else if redLightView.alpha == 1 {
+            yellowLightView.alpha = 1
+            redLightView.alpha = 0.3
+        }
+        else if yellowLightView.alpha == 1 {
+            greenLightView.alpha = 1
+            yellowLightView.alpha = 0.3
+        }
+        else {
+            redLightView.alpha = 1
+            greenLightView.alpha = 0.3
+        }
+    }
 }
 
